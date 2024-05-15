05ac9d87224314e7d1dc556cf5b36a8d09abf399
John McKisson <john.mckisson@gmail.com>
2024-05-15 09:03:58 -0400

    fix call for no pw

--- CI/deploy-mudlet-for-windows.sh
+++ CI/deploy-mudlet-for-windows.sh
@@ -258,7 +258,7 @@ else
   openssl req -x509 -sha256 -nodes -days 3650 -newkey rsa:4096 -keyout mykey.key -out mypem.pem -subj '/C=US/ST=VA/L=Yorktown/O=YourOrganization/OU=YourOrganizationalUnit/CN=jmckisson.com'
   
   echo "=== Converting cert to p12 ==="
-  openssl pkcs12 -export -out myp12.p12 -inkey mykey.key -in mypem.pem
+  openssl pkcs12 -export -out myp12.p12 -inkey mykey.key -in mypem.pem -passout pass:
 
   echo "=== Building the installer ==="
   # Execute Squirrel to create the installer
